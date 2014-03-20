7c33cdd8ec2f5a5f29d031ec8373e579dbedfae9
IAtilla <iatilla91@gmail.com>
2014-03-20 21:46:12 +0100

    Tower és Trap kommentezése.

--- skeleton/src/skeleton/Tower.java
+++ skeleton/src/skeleton/Tower.java
@@ -6,12 +6,30 @@ import java.util.List;
  * A torony megvalósításához használt osztály.
  */
 public class Tower extends EnemyObserver {
+	/*
+	 * A torony pozíciója.
+	 */
 	private Position position;
+	/*
+	 * A torony hatótávolsága.
+	 */
 	private double range;
+	/*
+	 * A toronyhoz tartozó mezõ referenciája.
+	 */
 	private Field field;
+	/*
+	 * A toronyhoz tartozó varázskövek.
+	 */
 	private List<GemStone> gemList;
+	/*
+	 * A torony hatósugarában lévõ ellenségek.
+	 */
 	private  List<Enemy> enemyList;
 	
+	/*
+	 * Konstruktor.
+	 */
 	public Tower() {
 		ConsoleUI.writeSeq("-->new Tower()");
 		ConsoleUI.writeSeq("<--Tower");
@@ -38,34 +56,58 @@ public class Tower extends EnemyObserver {
 		ConsoleUI.writeSeq("<--void");
 	}
 	
+	/*
+	 * A torony pozíciójának lekérdezése.
+	 */
 	public Position getPosition() {
 		return position;
 	}
 	
+	/*
+	 * A torony pozíciójának beállítása.
+	 */
 	public void setPosition(Position position) {
 	
 	}
 	
+	/*
+	 * A torony hatótávolságának lekérdezése.
+	 */
 	public double getRange() {
 		return range;
 	}
 	
+	/*
+	 * A torony hatótávolságának beállítása.
+	 */
 	public void setRange(double range) {
 	
 	}
 	
+	/*
+	 * A toronyhoz tartozó mezõ referenciájának beállítása.
+	 */
 	public void setField(Field field) {
 	
 	}
 	
+	/*
+	 * Új varázskõ hozzáadása.
+	 */
 	public void addGemStone(String gemstone) {
 	
 	}
 	
+	/*
+	 * A varázskövek listájának lekérdezése.
+	 */
 	public List<GemStone> getGemStoneList() {
 		return gemList;
 	}
 	
+	/*
+	 * 	Hozzáad egy ellenséget a listához.
+	 */
 	public void addEnemy(Enemy e) {
 		ConsoleUI.writeSeq("-->addEnemy(e: Enemy)");
 		if(!enemyList.contains(e)) {
@@ -74,6 +116,9 @@ public class Tower extends EnemyObserver {
 		ConsoleUI.writeSeq("<--void");
 	}
 	
+	/*
+	 * Eltávolít egy ellenséget a listáról.
+	 */
 	public void removeEnemy(Enemy e) {
 		ConsoleUI.writeSeq("-->removeEnemy(e: Enemy)");
 		if(enemyList.contains(e)) {
@@ -82,6 +127,9 @@ public class Tower extends EnemyObserver {
 		ConsoleUI.writeSeq("<--void");
 	}
 	
+	/*
+	 * Torony törlése.
+	 */
 	public void wipe() {
 		ConsoleUI.writeSeq("-->Tower.wipe()");
 		ConsoleUI.writeSeq("<--void");
--- skeleton/src/skeleton/Trap.java
+++ skeleton/src/skeleton/Trap.java
@@ -5,49 +5,93 @@ package skeleton;
  */
 
 public class Trap extends EnemyObserver {
+	/*
+	 * Az akadály megszûnésének ideje.
+	 */
 	private long endTime;
+	/*
+	 * Az akadály pozíciója.
+	 */
 	private Position position;
+	/*
+	 * Az akadályt tartalmazó útelem referenciája.
+	 */
 	private Road road;
+	/*
+	 * Az opcionális varázskõre mutató referencia.
+	 */
 	private GemStone gem;
 	
+	/*
+	 * Konstruktor.
+	 */
 	public Trap() {
 		ConsoleUI.writeSeq("-->new Trap()");
 		ConsoleUI.writeSeq("<--Trap");
 	}
 	
+	/*
+	 * Az ellenség ezen a metóduson keresztül értesíti a feliratkozott objektumokat.
+	 */
+	@Override
 	public void notifyFromEnemy(Enemy enemy) {
 	
 	}
 	
+	/*
+	 * A megszûnés idejének lekérdezése.
+	 */
 	public long getEndTime() {
 		return endTime;
 	}
 	
+	/*
+	 * A megszûnés idejének beállítása.
+	 */
 	public void setEndTime(long endtime) {
 	
 	}
 	
+	/*
+	 * A pozíció lekérdezése.
+	 */
 	public Position getPosition() {
 		return position;
 	}
 	
+	/*
+	 * A pozíció beállítása.
+	 */
 	public void setPosition(Position position) {
 	
 	}
 	
+	/*
+	 * Az akadályt tartalmazó útelem beállítása.
+	 */
 	public void setRoad(Road road) {
 	
 	}
 	
+	/*
+	 * Varázskõ hozzáadása.
+	 */
 	public void addGemStone(String gemstone) {
 	
 	}
 	
+	/*
+	 * Varázskõ létezésének lekérdezése.
+	 */
 	public boolean isGemStoned() {
 		//TODO
 		return false;
 	}
 
+	/*
+	 * Az akadály értesül róla, hogy egy ellenséges egység lépett rá,
+	 * és meg tudja tenni a szükséges lépéseket.
+	 */
 	public void enemyHasSteppedOn(Object e) {
 	
 	}
