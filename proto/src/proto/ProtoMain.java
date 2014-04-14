d143259ec8ea8c556498f30b07ef476fe09db74c
blackdog <blackdog@sch.bme.hu>
2014-04-14 21:00:26 +0200

    Skeleton gyomlalva

---
+++ proto/src/proto/ProtoMain.java
@@ -0,0 +1,12 @@
+package proto;
+
+/*
+ * A proto keretet ado osztaly, teszteles innet indul
+ */
+public class ProtoMain {
+	
+	static public void main(String[] args) {
+		System.out.println("Hello world");
+	}
+	
+}
