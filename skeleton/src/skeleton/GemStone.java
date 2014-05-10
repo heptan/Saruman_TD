0c82112499a26f16ccae4c97d03d68fbd8ff5ca7
BigBlackDog <cellosion@gmail.com>
2014-05-10 11:48:41 +0200

    Skeleton visszaallitva

---
+++ skeleton/src/skeleton/GemStone.java
@@ -0,0 +1,16 @@
+package skeleton;
+
+/*
+ * Varázskõvet megvalósító absztrakt õsosztály.
+ */
+public abstract class GemStone {
+
+	
+	/*
+	 * TODO Ez mire is kell pontosan? Milyen attribútumot állít be?
+	 */
+	public void setEffect() {
+		
+	}
+	
+}
---
+++ skeleton/src/skeleton/Hobbit.java
@@ -0,0 +1,13 @@
+package skeleton;
+
+/*
+ * A hobbit megvalósítására használt osztály.
+ */
+public class Hobbit extends Enemy {
+	/*
+	 * Hobbit léptetéséhez, pozícionálásáhor használt metódus.
+	 */
+	public void nextStep(){
+		
+	}
+}
