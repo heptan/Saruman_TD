d10d019e4679bb2be0351032961d6c99cefc7998
BigBlackDog <cellosion@gmail.com>
2014-05-11 15:48:17 +0200

    Comment++ (Constants)

--- proto/src/proto/Constants.java
+++ proto/src/proto/Constants.java
@@ -1,15 +1,37 @@
 package proto;
 
+/**
+ * A programban hasznalt konstansokat tarolo osztaly.
+ * 
+ * @author Alex Torok
+ */
 public class Constants {
 	
+	/**
+	 * A betoltendo terkepkepet tartalmazo fajl relativ el
+	 */
 	public static final String MAP_FILE_SRC = "map_l.test";
 	
+	/**
+	 * A tornyok alapertelmezett hatosugara
+	 */
+	public static final int TOWER_DEFAULT_RANGE = 2;
+	
+	/**
+	 * Kod eseten a tornyok hatosugara
+	 */
 	public static final int MIST_RANGE = 0;
 	
+	/**
+	 * Enemyk maximalis eletereje, ekkora eleterovel inicializalodnak
+	 */
 	public static final int ENEMY_MAX_HEALTH = 100;
+	
+	/**
+	 * Egy jatekban megjeleno ellensegek szama
+	 */
 	public static final int ENEMY_COUNTER_MAX = 10;
 
-	public static final int TOWER_DEFAULT_RANGE = 2;
 	public static final int TOWER_SHOT = 20;
 	public static final int TOWER_DAMAGEDWARF = 100;
 	public static final int TOWER_DAMAGEELF = 100;
